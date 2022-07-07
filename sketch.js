7fe5f88aeff758db0f0482f9b14712a772093f08
MatiasMantobani <matiasmantobani1@gmail.com>
2022-07-07 18:17:12 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,97 @@
+/*
+EL PUSH VA PRIMERO Y LUEGO EL NEW BOL, si lo haces al revez el primer elemento del array se buguea y queda con 2 propiedades del class sumadas? es algo raro ni idea
+*/
+
+
+//forma 2 (descargando fuente)
+// let myFont;
+// function preload() {
+//   myFont = loadFont('fonts/RubikMoonrocks-Regular.ttf');
+// }
+
+let bols = [];
+let texto = "Linked In 2022"
+let cantidad = 50;
+let cantidadMaxima = 400;
+
+function setup() {
+  createCanvas(windowWidth, windowWidth);
+}
+
+//creando bolas en donde esta maus
+function mousePressed() {
+  for (i = 0; i < cantidad; i++) {
+    bols.push(new BOL(mouseX, mouseY));
+  }
+}
+
+function draw() {
+  
+  // limitar cant de bolas totales
+if(bols.length > cantidadMaxima){
+bols.splice(0,cantidad)
+}
+  
+  background(0);
+  rectMode(CENTER);
+  stroke(255);
+  noFill();
+  rect(770, 570, 50, 50);
+  
+
+  fill(255);
+  noStroke();
+  textAlign(CENTER);
+  textSize(15);
+
+  // textFont(myFont); //forma 2
+  textFont("Rubik Moonrocks"); //forma 1
+  
+  text("Restart?",765,540);
+  
+  strokeWeight(1)
+    textSize(48)
+     text(texto,mouseX,mouseY);
+  
+  
+  for (i = 0; i < bols.length; i++) {
+    bols[i].show();
+    bols[i].move();
+    bols[i].bounce();
+    
+    
+  }
+}
+
+class BOL {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+    this.r = 25;
+    this.speedx = random(-3, 3);
+    this.speedy = random(-3, 3);
+    this.cl = random(["cyan", "red", "pink", "blue", "yellow", "green", "purple", "orange", "coral", "indigo", "white"])
+    this.cl = "white"
+    this.cl = random(["white","black"])
+  }
+
+  show() {
+    noFill();
+    stroke(this.cl);
+    strokeWeight(5);
+    ellipse(this.x, this.y, this.r);
+  }
+  move() {
+    this.x = this.x + this.speedx;
+    this.y = this.y + this.speedy;
+  }
+  bounce() {
+    if (this.x > width - this.r / 2 || this.x < 0 + this.r / 2) {
+      this.speedx = -this.speedx;
+    }
+
+    if (this.y > height - this.r / 2 || this.y < 0 + this.r / 2) {
+      this.speedy = -this.speedy;
+    }
+  }
+}
